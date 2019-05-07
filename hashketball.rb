84d6098d5642712a468217b8b4fa683d974a903a
Nia Bert <nbert14@gmail.com>
2019-05-07 01:20:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,5 +128,15 @@ def num_points_scored(name)
   end
 end
 
+def shoe_size(name)
+  hash = game_hash
+  hash.each do |location, info| 
+    info.each do |attribute, stuff| 
+      if stuff.include?(name) 
+       return hash[location][attribute][name][:shoe]
+      end
+    end
+  end
+end
 
 
