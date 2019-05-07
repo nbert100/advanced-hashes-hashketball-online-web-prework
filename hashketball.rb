48d4727917e4b86958000aa636b953527d30d436
Nia Bert <nbert14@gmail.com>
2019-05-07 01:24:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -143,7 +143,7 @@ def team_colors(team_name)
   hash = game_hash
   hash.each do |location, info| 
       if info.include?(team_name) 
-       return hash[location][info][:colors]
+       return hash[location][team_name][:colors]
       end
     end
   end
