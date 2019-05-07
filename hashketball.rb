6fcdaa24ba850d74cd6c7ab39e0d91e37847799f
Nia Bert <nbert14@gmail.com>
2019-05-07 00:34:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -98,7 +98,7 @@ def game_hash {
         :assists => 2,
         :steals => 1,
         :blocks => 1,
-        :slam_dunks => 
+        :slam_dunks => 0
       },
       "Brendan Haywood" => {
         :number => 33,
