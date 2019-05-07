a8d2b9cf7cc0ad8c3bbffe22547f52b71f3946bb
Nia Bert <nbert14@gmail.com>
2019-05-07 01:19:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -117,13 +117,15 @@ def game_hash
 }
 end
 
-def num_points_scored(players_name)
-  hash.each do |location, info|
-    info.each do |players, points|
-   puts points.value
+def num_points_scored(name)
+  hash = game_hash
+  hash.each do |location, info| 
+    info.each do |attribute, stuff| 
+      if stuff.include?(name) 
+       return hash[location][attribute][name][:points]
       end
     end
-  end.flatten.compact
+  end
 end
 
 
