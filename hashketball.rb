578e7e9d48ac20eb78aa4dca96f0dfd9369ddbed
Nia Bert <nbert14@gmail.com>
2019-05-07 00:14:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,14 @@
 # Write your code here!
-
+def game_hash {
+  :home => {
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"],
+    :players => {
+      
+    }
+  },
+  :away => {}
+}
 
 
 
